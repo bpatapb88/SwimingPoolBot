d1e8d6bfa434c66a11fe6b74dc2f24066ec04ab9
bpatapb88 <bpatapb87@gmail.com>
2024-01-12 14:21:05 +0100

    reduce java version to 11

--- src/main/java/com/simanov/leonSleep/SleepCommand.java
+++ src/main/java/com/simanov/leonSleep/SleepCommand.java
@@ -2,10 +2,22 @@ package com.simanov.leonSleep;
 
 import java.time.LocalTime;
 
-public record SleepCommand(
-        LocalTime time,
-        String command
-) {
+public class SleepCommand {
+    private LocalTime time;
+    private String command;
+
+    public SleepCommand(LocalTime time, String command) {
+        this.time = time;
+        this.command = command;
+    }
+
+    public LocalTime time() {
+        return time;
+    }
+
+    public String command() {
+        return command;
+    }
 
     @Override
     public String toString() {
